9ced724acd9b6ea80294e6a4a471c1120c89d258
Yumenosuke Kokata <y-kokata@r-up.jp>
2019-01-22 10:31:36 +0900

    Initial Commit

---
+++ Storyboard Referencing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storyboard Referencing
+//
+//  Created by yumenosuke koukata on 2019/01/22.
+//  Copyright © 2019 yumenosuke koukata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
